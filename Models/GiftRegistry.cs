a4d8a97516cdf99ede0c0d501adf00d1fdb90289
GabrielFracalossi <g4brielfnazario@gmail.com>
2025-03-02 14:21:45 -0300

    fix: resolve build erros

--- Models/GiftRegistry.cs
+++ Models/GiftRegistry.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WeddingPlatform.Models
 {
     public class GiftRegistry
@@ -7,6 +9,7 @@ namespace WeddingPlatform.Models
         public virtual WeddingSite WeddingSite { get; set; }
         public string ItemName { get; set; }
         public string Description { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public bool IsPurchased { get; set; }
