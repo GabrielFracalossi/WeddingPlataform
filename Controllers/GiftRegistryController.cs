d53021d5ce9999b3edf14d60f8dfebab327b7c3f
GabrielFracalossi <g4brielfnazario@gmail.com>
2025-03-03 01:10:56 -0300

    fix: Adjust page's navigations

--- Controllers/GiftRegistryController.cs
+++ Controllers/GiftRegistryController.cs
@@ -1,12 +1,25 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WeddingPlatform.Data;
+using WeddingPlatform.Models;
 
 namespace WeddingPlatform.Controllers
 {
+    [Authorize]
     public class GiftRegistryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public GiftRegistryController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var gifts = await _context.GiftRegistry.ToListAsync();
+            return View(gifts);
         }
     }
 }
